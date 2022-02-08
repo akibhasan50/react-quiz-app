757ed2bcee6a6174c54fc95b8712c0f606520f41
akibhasan50 <tonuakibhasan@gmail.com>
2022-02-08 23:35:43 +0600

    wrapping up

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,6 +1,31 @@
 import classes from '../styles/Summary.module.css'
 import img from '../assets/images/success.png'
+import useFetch from '../hooks/useFetch';
+import { useMemo } from 'react';
 export default function Summary({score,noq}) {
+
+  const getKeyword = useMemo(() => {
+    if ((score / (noq * 5)) * 100 < 50) {
+      return "failed";
+    } else if ((score / (noq * 5)) * 100 < 75) {
+      return "good";
+    } else if ((score / (noq * 5)) * 100 < 100) {
+      return "very good";
+    } else {
+      return "excellent";
+    }
+  }, [score, noq]);
+
+  const { loading, error, result } = useFetch(
+    `https://api.pexels.com/v1/search?query=${getKeyword}&per_page=1`,
+    "GET",
+    {
+      Authorization:'563492ad6f9170000100000154ac85380fcb476382227d21c4d78877',
+    }
+  );
+
+  const image = result ? result?.photos[0].src.medium : img;
+
     return (
         <div class={classes.summary}>
           <div class={classes.point}>
@@ -11,9 +36,15 @@ export default function Summary({score,noq}) {
             </p>
           </div>
 
-          <div class={classes.badge}>
-            <img src={img} alt="Success" />
-          </div>
+          {loading && <div className={classes.badge}>Loading your badge...</div>}
+
+{error && <div className={classes.badge}>An error occured!</div>}
+
+{!loading && !error && (
+  <div className={classes.badge}>
+    <img src={image} alt="Success" />
+  </div>
+)}
         </div>
 
     )
